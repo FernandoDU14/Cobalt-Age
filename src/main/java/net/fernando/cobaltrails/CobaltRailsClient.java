274f48125beaf686a664721f9242b320d5a9f219
javif <javifsobrino14@gmail.com>
2026-04-18 16:31:02 +0200

    Initial commit

---
+++ src/main/java/net/fernando/cobaltrails/CobaltRailsClient.java
@@ -0,0 +1,10 @@
+package net.fernando.cobaltrails;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobaltRailsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/fernando/cobaltrails/CobaltRailsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.fernando.cobaltrails;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CobaltRailsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
