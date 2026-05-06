1e46b73e9d8a93bf9675329764d7e3b39c711696
marco <marco.giacovazzo@gmail.com>
2026-05-06 23:16:10 +0200

    v1.0.2: (e) Comment Change

--- src/main/java/net/fernando/cobaltage/mixin/emitters/CalibratedSculkSensorCallbackMixin.java
+++ src/main/java/net/fernando/cobaltage/mixin/emitters/CalibratedSculkSensorCallbackMixin.java
@@ -11,7 +11,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-// NOTA: Il target deve essere la classe interna Callback
 @Mixin(targets = "net.minecraft.block.entity.CalibratedSculkSensorBlockEntity$Callback")
 public class CalibratedSculkSensorCallbackMixin {
 
